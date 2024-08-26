ba4d39b0d2ab37745ab19b1131dd0101d79f5b02
mortevs <86227643+mortevs@users.noreply.github.com>
2024-08-26 10:26:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,9 @@ def plot(df):
         showlegend=True,
         xaxis_title="S.G.",  # X-axis title
         yaxis_title="Depth",            # Y-axis title
+        yaxis=dict(
+        autorange='reversed'  # Reverse the y-axis
+    ),
         height=850
     )
     fig = st.plotly_chart(fig, use_container_width=True)
@@ -108,6 +111,9 @@ def plot_with_points(df2, df):
         showlegend=True,
         xaxis_title="S.G.",  # X-axis title
         yaxis_title="Depth",            # Y-axis title
+        yaxis=dict(
+        autorange='reversed'  # Reverse the y-axis
+    ),
         height=850,
     )
     fig = st.plotly_chart(fig, use_container_width=True)
