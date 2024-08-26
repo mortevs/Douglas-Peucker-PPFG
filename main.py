1315a2bd1b3bbec4040d8e82ec488304b8131377
mortevs <86227643+mortevs@users.noreply.github.com>
2024-08-26 10:19:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def plot(df):
         showlegend=True,
         xaxis_title="S.G.",  # X-axis title
         yaxis_title="Depth",            # Y-axis title
-        yaxis=dict(autorange='reversed')
+        yaxis=dict(autorange='reversed'),
         height=850
     )
     fig = st.plotly_chart(fig, use_container_width=True)
@@ -107,9 +107,9 @@ def plot_with_points(df2, df):
             buttons=all_buttons
         )],
         showlegend=True,
-        xaxis_title="Pressure [bara]",  # X-axis title
+        xaxis_title="S.G.",  # X-axis title
         yaxis_title="Depth",            # Y-axis title
-        yaxis=dict(autorange='reversed')
+        yaxis=dict(autorange='reversed'),
         height=850
     )
     fig = st.plotly_chart(fig, use_container_width=True)
